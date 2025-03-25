355a987ff7b0d495137bc1417a58e0f1228170eb
talus999 <jtrejopre05@gmail.com>
2025-03-25 18:55:11 +0100

    0.2.1

--- Combate.js
+++ Combate.js
@@ -23,12 +23,23 @@ class Combate{
     }
 
     turnoJugador(){
-        this.personaje.atacar(this.enemigo)
-        if (this.enemigo.vida <= 0){
-            console.log(`Has ganado`)
-            return;
+        let opcion = prompt("¿Qué quieres hacer? \n1. Atacar\n2. Huir");
+
+        if (opcion === "1"){
+            this.personaje.atacar(this.enemigo)
+            if (this.enemigo.vida <= 0){
+                console.log(`Has ganado`)
+                return;
+            }
+            this.turno = "Enemigo";
+        } else if (opcion === "2") {
+            this.huir();
+        } else {
+            console.log("Eso no existe, imbecil. Pierdes tu turno.");
+            this.turno = "Enemigo";
         }
-        this.turno = "Enemigo";
+        
+        
     }
 
     turnoEnemigo(){
@@ -38,6 +49,17 @@ class Combate{
             return;
         }
         this.turno = "Personaje";
+    }
+
+    huir() {
+        let probabilidadEscape = this.personaje.velocidad / (this.personaje.velocidad + this.enemigo.velocidad);
+        if (Math.random() < probabilidadEscape) {
+            console.log("Has logrado escapar del combate.");
+            return
+        } else {
+            console.log("No has podido escapar.");
+            this.turno = "Enemigo";
+        }
     }
 
 }
